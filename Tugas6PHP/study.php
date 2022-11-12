e34764e23a7f341fc8b07b0ed42b08df94440978
ahmadzami123 <93628141+ahmadzami123@users.noreply.github.com>
2022-11-12 12:20:10 +0700

    Add files via upload

---
+++ Tugas6PHP/study.php
@@ -0,0 +1,31 @@
+<!-- ======= Resume Section ======= -->
+<section id="resume" class="resume">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Study</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="resume-item">
+              <h4>MI Amanah Ruteng</h4>
+              <h5>2007 - 2013</h5>
+            </div>
+            <div class="resume-item">
+              <h4>MTS Amanah Ruteng</h4>
+              <h5>2013 - 2015</h5>
+            </div>
+            <div class="resume-item">
+              <h4>MA Al Haitsam Bogor</h4>
+              <h5>2016 - 2019</h5>
+            </div>
+            <div class="resume-item">
+              <h4>Teknik Informatika - Universitas Muhammadiyah Surakarta</h4>
+              <h5>2019 - Sekarang</h5>
+            </div>
+          
+        </div>
+
+      </div>
+    </section><!-- End Resume Section -->
